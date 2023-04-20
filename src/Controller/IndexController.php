24dd4970de6c24e75a58ac0753029ad22381884f
Yury Potapov <yury.potapov@satellite-soft.ru>
2023-04-20 13:59:58 +0300

    Added example to use routing module

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,14 +3,18 @@
 namespace App\Controller;
 
 use Mubiridziri\Geocenter\Model\DecoderContext;
+use Mubiridziri\Geocenter\Model\Direction;
 use Mubiridziri\Geocenter\Model\LatLng;
 use Mubiridziri\Geocenter\Model\ReverseDecoderContext;
+use Mubiridziri\Geocenter\Model\VehicleOptions;
 use Mubiridziri\Geocenter\Option\GeodecodeData;
 use Mubiridziri\Geocenter\Option\GeodecodeFormat;
 use Mubiridziri\Geocenter\Option\GeodecodeType;
+use Mubiridziri\Geocenter\Option\VehicleType;
 use Mubiridziri\Geocenter\Service\GeocenterManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 
@@ -42,5 +46,19 @@ class IndexController extends AbstractController
             ->setType(GeodecodeType::ROAD)
         );
         return $this->json($results);
+    }
+
+    /**
+     * Построение маршрута
+     * @return Response
+     * @Route("/route", methods={"GET"})
+     */
+    public function routingAction(GeocenterManager $manager): Response
+    {
+        $geoJson = $manager->getRoute((new Direction())
+            ->setPoints([['x' => 59.13026, 'y' => 53.36315], ['x' => 59.07184, 'y' => 53.38607]])
+            ->setVehicles([VehicleType::CAR => new VehicleOptions()])
+        );
+        return $this->json($geoJson);
     }
 }
